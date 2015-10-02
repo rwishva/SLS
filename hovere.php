15cac7058556a28a3136ba7a8decc6cca26045a8
rwishva <rwishva@gmail.com>
2015-10-02 17:28:52 +0530

    10

--- hovere.php
+++ hovere.php
@@ -6,205 +6,104 @@
 <html lang="en">
   <head>
     <meta charset="utf-8" />
-    <title>jQuery UI Selectable - Display as grid</title>
+    <title>MC - Select Pixels</title>
     <link href='/SLS/css/bootstrap.min.css' rel='stylesheet'>
     <link rel='stylesheet' type='text/css' href='/SLS/css/bstyle.css' />
-
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-
-<style>
-  #feedback { font-size: 1.4em; }
-  #Map .ui-selecting { background-image: url("A.png"); }
-  #Map .ui-selected { background-image: url("R.png"); color: white; }
-  #Map { list-style-type: none; margin: 0; padding: 0; width: 450px; }
-  #Map area { margin: 0px; padding: 0px; float: left; width: 10px; height: 10px; font-size: 4em; text-align: center; }
-  /*center {min-width: 1000px;min-height: 1000px;}*/
-   #Map .ui-state-default{border: 0;}
-   /*#Map .ui-state-default{border: 0; background-image: url("A.png"); z-index: inherit;}*/
-   .box {
-    /*background-color: lightgrey;
-        background: url(bodybg.gif) repeat #FFFFFF;*/
-    width: 1000px;
-    height: 1000px;
-    /*padding: 25px;*/
-    border: 1px solid #BABABB;
-    margin: 25px;
-    margin-top: 0;
-    vertical-align: middle;
-}
-#Map .available { background-image: url("A.png"); }
-#Map .reserved { background-image: url("R.png"); }
+    <link rel="SHORTCUT ICON" href="fav-2.png">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <link rel='stylesheet' type='text/css' href='/SLS/css/style_hove.css' />
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 
 
-  .box {
-      background-color: lightgrey;
-      background: url(bodybg.gif) repeat #FFFFFF;
-      width: 1003px;
-      height: 1003px;
-      /*padding: 25px;*/
-      border: 1px solid #BABABB;
-      margin: 25px;
-      margin-top: 0;
-      vertical-align: middle;
-  }
-  body{
-    background: url(bodybg.gif) repeat #FFFFFF;
-  }
-  .SLS{
-    background-color: black;
-  }
-  #indexbtn {
-      cursor: pointer;
-      display: inline-block;
-      color: #fff;
-      line-height: 1;
-      padding: .6em .8em;
-      background: #009afd;
-      -webkit-transition: background 0.15s ease, color 0.15s ease;
-      -moz-transition: background 0.15s ease, color 0.15s ease;
-      -ms-transition: background 0.15s ease, color 0.15s ease;
-      -o-transition: background 0.15s ease, color 0.15s ease;
-      border: 1px solid #1777b7;
-      box-shadow: 0 1px 0 rgba(255,255,255,0.3) inset,0 1px 1px rgba(100,100,100,0.3);
-      border-radius: 3px;
+  <script>
+  var selectedpxs = [];
+  var pxs;
+    $(function() {
+      $( "#Map" ).selectable({
+        selected: function( event, ui ) {
 
-    }
-  #describe{
-      color: white;
-    }
-    a{
-      color: #FFD700;
-    }
-    #footer{
-          margin-bottom: 0;
-          height: 10px;
-          min-height: 10px;
-          background-color: transparent;  
-    }
-    #footer a{
-      color: #BABABB;
-    }
-
-  table, th, td {
-      border: 1px solid #8E8E8E;
-  }
-  tr{
-    height: 10px;
-  }
-  #pixl{
-    position: relative;
-    clip: rect(5, 1, 5, 1);
-    border: none;
-    float: left;
-    
-  }
-  #FAQ
-  {
-    margin-top: -5px;
-    margin-right: 5px;
-  }
-
-  a.login {
-    text-decoration: none;
-    background: #636363;
-    padding: .5em .5em .5em .5em;
-    float: right;
-    margin: -8px 0px -6px 0;
-    color: #BAC2C5;
-    font-size: 12px; }
-  a.login:hover {
-      color: #EFD700; 
-    }
-  a#brand:hover{
-      /*color: #ffd999;*/
-        text-decoration: none;
-    color: #FFD700;
-      text-shadow: -1px 1px 40px #ffc, 1px -1px 40px #fff;
-    -webkit-transition: 500ms linear 0s;
-    -moz-transition: 500ms linear 0s;
-    -o-transition: 500ms linear 0s;
-    transition: 500ms linear 0s;
-    outline: 0 none;
-    }
-  /* clip: shape(top, right, bottom, left); NB 'rect' is the only available option */
-  .description {
-      display:none;
-      position:absolute;
-      border:1px solid #FFF;
-      background-color: green;
+          var pxs = $(ui.selected).attr('id');
+          selectedpxs.push(pxs);
      
-  }
-  div#pop-up{
-    display: none;
-    position: absolute;
-    background: black;
-    padding: 0;
-    margin: 0;
-    border: 1px solid #FFD700;
-    color: #FFD700;
-    /*height: 14px;*/
-  }
-
-  
-
-
-  </style>
-<script>
-var selectedpxs = [];
-var pxs;
-  $(function() {
-    $( "#Map" ).selectable({
-      selected: function( event, ui ) {
-        // alert( event.pageX)
-
-        var pxs = $(ui.selected).attr('id');
-        selectedpxs.push(pxs);
-        // selectedpxs.length = 0;
-        // console.log(fruits);
-        // delete fruits;
-        //cars ="";
-        // alert();
-        // var idlist = $(this).attr('id');
-        // alert(idlist);
-      }
-    });
-  });
-
-
-// var sTitle="";
-// function d(o) {
-//   alert(fruits);
-//   // sTitle = o.id;
-// }
-// function e(o) {
-//   sTitle = "";  
-// }
-function updateselected()
-{
-dataString = selectedpxs ; // array?
-var jsonString = JSON.stringify(dataString);
-   $.ajax({
-        type: "POST",
-        url: "updateselected.php",
-        data: {data : jsonString}, 
-        cache: false,
-        dataType: 'json',
-
-        success: function(data){
-            // window.location='millioncompany.php'
-             // var $response=$(data);
-             if(data.type == 'success') {
-                   window.location.href = "pricing.php?pxset=" + data.message;
-                }
-            
         }
+      });
+
+
+
+  $( "#Map" ).selectable({
+  start: function( event, ui ) {
+          selectedpxs = [];
+        }
+        });
+
+
+  $( "#Map" ).selectable({
+  stop: function( event, ui ) {
+          
+          $("#popup_div").dialog({ autoOpen: false });
+          document.getElementById("aaa").innerHTML = selectedpxs;
+          $( "#popup_div" ).dialog({
+            dialogClass: "alert",
+            title: "Pixel selection"
+          });
+          $("#popup_div").dialog("open");
+        }
+        });
+
     });
-}
-</script>
+
+  function closebox()
+  {
+    $("#popup_div").dialog("close"); 
+      
+    $( "#Map" ).selectable( "refresh" ); 
+  }
+
+  // var sTitle="";
+  // function d(o) {
+  //   alert(fruits);
+  //   // sTitle = o.id;
+  // }
+  // function e(o) {
+  //   sTitle = "";  
+  // }
+      function updateselected()
+      {
+      dataString = selectedpxs ; // array?
+      var jsonString = JSON.stringify(dataString);
+         $.ajax({
+              type: "POST",
+              url: "updateselected.php",
+              data: {data : jsonString}, 
+              cache: false,
+              dataType: 'json',
+
+              success: function(data){
+                  // window.location='millioncompany.php'
+                   // var $response=$(data);
+                   if(data.type == 'success') {
+                         window.location.href = "pricing.php?pxset=" + data.message;
+                      }
+                  
+              }
+          });
+      }
+  </script>
 </head>
 <body>
+
+
+<div id="popup_div" class="alert">
+<center>
+<a href="" class="pull-center">You Have Selected Following Pixels</a>
+<p href="" id="aaa">You Have Selected</p>
+  <button class="btn btn-default navbar-btn" id="gold" type="button" onclick="updateselected()">Reserve</button>
+  <button class="btn btn-default navbar-btn" id="gold" type="button" onclick="closebox()">Reset</button>
+  </center>
+  
+</div>
+<div id="fade" class="black_overlay"></div>
 
 <center>
   <nav class="navbar SLS">
@@ -257,7 +156,7 @@ var jsonString = JSON.stringify(dataString);
 
 
 <center>
-<button type="button" onclick="updateselected()">Reserve</button>
+
             <div class="box">
               <table>
               <tr>
